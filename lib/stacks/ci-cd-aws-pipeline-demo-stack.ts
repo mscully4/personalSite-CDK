257ce4297bf749812ad7accf4c47bfdd302e24f1
Michael <michael.scully1997@gmail.com>
2022-08-21 18:25:11 -0400

    bar

--- lib/stacks/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/stacks/ci-cd-aws-pipeline-demo-stack.ts
@@ -17,7 +17,7 @@ export class CiCdAwsPipelineDemoStack extends cdk.Stack {
       pipelineName: "TestPipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("mscully4/personalSite-CDK", "test"), //Remember to change
-        commands: ["npm ci", "npm run build", "npx cdk synth"],
+        commands: ["npm ci", "npm run build", "npx cdk synth", "ls"],
         additionalInputs: {
           api: CodePipelineSource.gitHub(
             "mscully4/personalSite-Backend",

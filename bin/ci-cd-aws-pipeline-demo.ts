df73d572c8e038960ff5bb245046556deebdd8be
Michael <michael.scully1997@gmail.com>
2022-08-21 16:54:43 -0400

    foo

--- bin/ci-cd-aws-pipeline-demo.ts
+++ bin/ci-cd-aws-pipeline-demo.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CiCdAwsPipelineDemoStack } from '../lib/ci-cd-aws-pipeline-demo-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CiCdAwsPipelineDemoStack } from "../lib/ci-cd-aws-pipeline-demo-stack";
 
 const app = new cdk.App();
-new CiCdAwsPipelineDemoStack(app, 'CiCdAwsPipelineDemoStack', {
+new CiCdAwsPipelineDemoStack(app, "CiCdAwsPipelineDemoStack", {
   env: {
-    account: '755314965794',
-    region: 'us-east-1',
-  }
+    account: "735029168602",
+    region: "us-west-2",
+  },
 });
 
 app.synth();
--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,35 +1,43 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
-import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-import { MyPipelineAppStage } from './stage';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+  Step,
+} from "aws-cdk-lib/pipelines";
+import { ManualApprovalStep } from "aws-cdk-lib/pipelines";
+import { MyPipelineAppStage } from "./stage";
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'TestPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('beabetterdevv/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
-        commands: ['npm ci', 
-                   'npm run build', 
-                   'npx cdk synth']
-      })
+    new CodePipeline(this, "Pipeline", {
+      pipelineName: "TestPipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("mscully4/personalSite-CDK", "test"), //Remember to change
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
 
+    const testingStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "test", {
+        env: { account: "735029168602", region: "us-west-2" },
+      })
+    );
 
+    testingStage.addPre(
+      new ShellStep("Run Unit Tests", { commands: ["npm install", "npm test"] })
+    );
+    testingStage.addPost(
+      new ManualApprovalStep("Manual approval before production")
+    );
 
-    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
-      env: { account: "755314965794", region: "us-east-1" }
-    }));
-
-
-    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
-    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
-
-    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
-      env: { account: "755314965794", region: "us-east-1" }
-    }));
+    const prodStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "prod", {
+        env: { account: "735029168602", region: "us-west-2" },
+      })
+    );
   }
 }
